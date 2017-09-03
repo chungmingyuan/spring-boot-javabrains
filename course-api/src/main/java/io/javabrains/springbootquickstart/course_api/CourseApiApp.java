3bcd51c27756292cd864205245ac90b7e05747ec
suncap <suncap11@gmail.com>
2017-09-03 19:23:12 +0300

    First Hello controller

---
+++ course-api/src/main/java/io/javabrains/springbootquickstart/course_api/CourseApiApp.java
@@ -0,0 +1,17 @@
+package io.javabrains.springbootquickstart.course_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class CourseApiApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiApp.class, args);
+	}
+	
+}
