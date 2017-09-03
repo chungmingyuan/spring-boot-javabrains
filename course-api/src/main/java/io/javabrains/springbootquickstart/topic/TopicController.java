05d48ba053a34c0062d9976088743d87afe42640
suncap <suncap11@gmail.com>
2017-09-03 22:18:35 +0300

    Edit and delete topic

--- course-api/src/main/java/io/javabrains/springbootquickstart/topic/TopicController.java
+++ course-api/src/main/java/io/javabrains/springbootquickstart/topic/TopicController.java
@@ -29,4 +29,14 @@ public class TopicController {
 	public void addTopic(@RequestBody Topic topic) {
 		topicService.addTopic(topic);
 	}
+	
+	@RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic, @PathVariable("id") String id) {
+		topicService.updateTopic(id, topic);
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
+	public void deleteTopic(@PathVariable("id") String id) {
+		topicService.deleteTopic(id);
+	}
 }
